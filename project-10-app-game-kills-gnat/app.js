d488537963eb88638a6f78d4f82a2bf36b9bf518
rochads <72424771+rochads@users.noreply.github.com>
2023-01-17 12:38:03 -0300

    project-10: created random positions

--- project-10-app-game-kills-gnat/app.js
+++ project-10-app-game-kills-gnat/app.js
@@ -5,8 +5,32 @@ function adjustGameWindowSize() {
     height = window.innerHeight
     width = window.innerWidth
 
-    console.log(height, width)
+    console.log(`window width: ${width}, window height: ${height}`)
 }
 
 adjustGameWindowSize()
+
+
+
+
+let positionX = Math.floor(Math.random() * width) - 90
+let positionY = Math.floor(Math.random() * height) - 90
+
+positionX = positionX < 0 ? 0 : positionX
+positionY = positionY < 0 ? 0 : positionY
+
+console.log (`gnat's position on the x-axis: ${positionX}, gnat's position on the y-axis: ${positionY}`)
+
+
+
+
+let gnat = document.createElement('img')
+gnat.src = "./resources/images/gnat.png"
+gnat.className = "gnat"
+gnat.style.left = `${positionX}px`
+gnat.style.top = `${positionY}px`
+gnat.style.position = 'absolute'
+
+document.body.appendChild(gnat)
+
 
