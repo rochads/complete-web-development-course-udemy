8e30bd518510f3cfa3f5bb03ec0c3a3032b71adb
rochads <72424771+rochads@users.noreply.github.com>
2023-01-17 11:52:08 -0300

    project-10 adjusted game window size

---
+++ project-10-app-game-kills-gnat/app.js
@@ -0,0 +1,12 @@
+let height = 0
+let width = 0
+
+function adjustGameWindowSize() {
+    height = window.innerHeight
+    width = window.innerWidth
+
+    console.log(height, width)
+}
+
+adjustGameWindowSize()
+
