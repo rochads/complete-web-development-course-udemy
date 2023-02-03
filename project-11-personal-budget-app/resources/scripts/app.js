6f31ecc48786f85e869ec03c6775655d2c3a0271
rochads <72424771+rochads@users.noreply.github.com>
2023-02-03 12:36:49 -0300

    created bd {} class

--- project-11-personal-budget-app/resources/scripts/app.js
+++ project-11-personal-budget-app/resources/scripts/app.js
@@ -11,6 +11,36 @@ class Expense {
 
 }
 
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getNextId() {
+        let next_id = localStorage.getItem('id')
+
+        return parseInt(next_id) + 1
+    }
+
+    write(e) {
+
+        let id = this.getNextId()
+
+        localStorage.setItem(id, JSON.stringify(e))
+
+        localStorage.setItem('id', id)
+    
+    }
+
+}
+
+let bd = new Bd()
+
 function registerExpense() {
 
     let year = document.getElementById('year')
@@ -29,12 +59,6 @@ function registerExpense() {
         value.value
     )
 
-    write(expense)
+    bd.write(expense)
     
-}
-
-function write(e) {
-
-    localStorage.setItem('expense', JSON.stringify(e))
-
 }
